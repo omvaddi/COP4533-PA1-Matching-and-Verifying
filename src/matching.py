ea0c66e547e7c6a35857244cf645042f1c390e5f
Om Vaddi <om.r.vaddi@gmail.com>
2026-01-28 17:41:30 -0500

    implemented gale-shapely algorithm

--- src/matching.py
+++ src/matching.py
@@ -2,17 +2,52 @@ def read_input(filename):
     with open(filename, 'r') as f:
         n = int(f.readline().strip())
 
-        hospitals = []
+        hospital_preferences = []
         for _ in range(n):
-            hospitals.append(list(map(int, f.readline().strip().split())))
+            hospital_preferences.append(list(map(int, f.readline().strip().split())))
         
-        students = []
+        student_preferences = []
         for _ in range(n):
-            students.append(list(map(int,f.readline().strip().split())))
+            student_preferences.append(list(map(int,f.readline().strip().split())))
 
-    return n, hospitals, students
+    return n, hospital_preferences, student_preferences
             
+def gale_shapley(filename):
+    n, hospital_preferences, student_preferences = read_input(filename)
+
+    hospitals = [-1] * n
+    students = [-1] * n
+
+    free_hospitals = list(range(n))
+
+    next_proposal = [0] * n
+
+    while free_hospitals:
+        h = free_hospitals.pop(0)
+
+        s = hospital_preferences[h][next_proposal[h]]
+        next_proposal[h] += 1
+
+        if students[s] == -1:
+            hospitals[h] = s
+            students[s] = h
+        else:
+            current_hospital = students[s]
+            if student_preferences[s].index(h) < student_preferences[s].index(current_hospital):
+                hospitals[h] = s
+                students[s] = h
+                hospitals[current_hospital] = -1
+                free_hospitals.append(current_hospital)
+            else:
+                free_hospitals.append(h)
+        
+    return hospitals, students
+        
+        
+
+
+
 
 
 if __name__ == "__main__":
-    print(read_input('tests/test1.txt'))
+    print(gale_shapley('tests/test1.txt'))
