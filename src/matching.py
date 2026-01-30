e391e2e19719770f6a7b57247a2783186aed2d59
alvaradot <alvaradot@ufl.edu>
2026-01-29 21:17:28 -0500

    added verifier function for validity and stability

--- src/matching.py
+++ src/matching.py
@@ -50,7 +50,45 @@ def gale_shapley(filename):
     for i in range(len(hospitals)):
         print(i, hospitals[i])
 
+    return hospitals, students, hospital_preferences, student_preferences
 
+def verifier(hospitals, students, hospital_preferences, student_preferences):
+    n = len(hospitals)
+
+    if -1 in hospitals:
+        return False, "INVALID: Unmatched hospital"
+    
+    if -1 in students:
+        return False, "INVALID: Unmatched student"
+    
+    if(len(set(hospitals))) != n:
+        return False, "INVALID: Student matched to multiple hospitals"
+    
+    if(len(set(students))) != n:
+        return False, "INVALID: Hospital matched to multiple students"
+    
+    hospital_ranks = [[0] * n for _ in range(n)]
+    student_ranks = [[0] * n for _ in range(n)]
+
+    for h in range(n):
+        for rank, s in enumerate(hospital_preferences[h]):
+            hospital_ranks[h][s] = rank
+
+    for s in range(n):
+        for rank, h in enumerate(student_preferences[s]):
+            student_ranks[s][h] = rank
+
+    for h in range(n):
+        current_student = hospitals[h]
+        for s in range(n):
+            if s == current_student:
+                continue
+            if hospital_ranks[h][s] < hospital_ranks[h][current_student]:
+                current_hospital = students[s]
+                if student_ranks[s][h] < student_ranks[s][current_hospital]:
+                    return False, f"UNSTABLE: hospital {h}, student {s}"
+                
+    return True, "VALID STABLE"
 
 if __name__ == "__main__":
     gale_shapley('tests/test1.txt')
